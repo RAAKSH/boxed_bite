8c63d4df31d575838e0b9d893d6ecfb92d228c85
Raksha <rakshac29@gmail.com>
2024-11-09 16:59:37 +0530

    Episode 2 ignite react app

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hello world from React"
-  );
-  
-  const root = ReactDOM.createRoot(document.getElementById("parent"));
-  root.render(heading);
-  
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React"
+// );
+
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1> I am an H1 Tag</h1>
+    <h2> I am an H2 Tag</h2>
+  </div>
+</div> */
+}
+
+// const heading1 = React.createElement(
+//   "div",
+//   {},
+//   React.createElement("div", {}, [
+//     React.createElement("h1", {}, "NAMSTE REACT abc "),
+//     React.createElement("h2", {}, "I am a h2 tag"),
+//   ])
+// );
+
+// console.log("abc", heading1);
+
+// const root = ReactDOM.createRoot(document.getElementById("parent"));
+// root.render(heading1);
+
+
+const headings=React.createElement("h5",{id:"heading"},"hai Raksha");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(headings);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # namaste-react
+
+# Parcel
+
+- Dev build
+- Local server
+- automatic refresh of browser
+- HMR (Hot Replacement Module)
+- Image processing
+- bundling
+- minify of file
+- compress
+- File watching algorithm
+- Caching
+- consitent hashing
+- code splitting
+- Differential Bindling (support older veriosn of brower)
+- Diagnostic
+- Error jandling
+- Tree shaking
