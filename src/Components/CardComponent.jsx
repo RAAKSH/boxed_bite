d4005f90f5d42222bc57d0d88fff701e6d023cc8
Raksha <rakshac29@gmail.com>
2025-06-19 12:22:44 +0530

    fixed card height

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -4,17 +4,25 @@ export const CardComponent = (props) => {
   const { info } = props?.resData?.card?.card;
 
   return (
-    <div className="m-2 p-2 w-[250px] bg-gray-100 hover:bg-slate-300 border border-solid rounded-m shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
-      <img
-        src={IMAGE_URL + info?.cloudinaryImageId}
-        alt="res-logo"
-        className="px-0.5 object-cover w-80 h-60  rounded-lg"
-      />
-      <h3 className="font-bold py-3 text-xl">{info?.name}</h3>
-      <h4 className="font-light">{info?.cuisines.slice(0, 4).join(", ")}</h4>
-      <h4 className="font-light">{info?.avgRating} Stars</h4>
-      <h4 className="font-light">{info?.sla?.deliveryTime} minutes</h4>
+    <div className="m-2 p-2 w-[250px] h-[380px] bg-gray-100 hover:bg-slate-300 border border-solid rounded-md shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl flex flex-col">
+    <img
+      src={IMAGE_URL + info?.cloudinaryImageId}
+      alt="res-logo"
+      className="object-cover w-full h-48 rounded-lg"
+    />
+    <div className="flex flex-col justify-between flex-grow mt-2">
+      <div className="space-y-1">
+        <h3 className="font-bold text-xl truncate">{info?.name}</h3>
+        <h4 className="font-light text-sm text-gray-700 truncate">
+          {info?.cuisines?.slice(0, 4)?.join(", ")}
+        </h4>
+        <h4>{info?.avgRating} Stars</h4>
+        <h4>{info?.sla?.deliveryTime} minutes</h4>
+      </div>
+  
+     
     </div>
+  </div>
   );
 };
 
